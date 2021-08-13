3c588a26b0e2c0cd4646ba66076cea7a835754de
acopperlily <acopperlily@yahoo.com>
2021-08-13 05:25:01 -0400

    Added functions to draw new grid

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let size = 16;
+let size = 36;
 
 function createCells() {
   let cell = document.createElement('div');
@@ -10,21 +10,33 @@ function createCells() {
 }
 
 const grid = document.querySelector('#grid');
+grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
 for (let i = 0; i < size * size; i++) {
   grid.appendChild(createCells());
 }
 
-grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+function newGrid(grid, size) {
+  for (let i = 0; i < size * size; i++) {
+    grid.appendChild(createCells());
+  }
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+}
 
 function draw(e) {
   e.style.backgroundColor = "black";
 }
 
 function clearCells() {
-  console.log('wut');
   let cell = document.getElementsByClassName('cell');
   for (let i = 0; i < size * size; i++) {
     cell[i].style.backgroundColor = 'red';
+  }
+}
+
+function deleteCells() {
+  while (grid.firstElementChild) {
+    grid.removeChild(grid.firstElementChild);
   }
 }
 
@@ -36,5 +48,8 @@ function getSize() {
 const btn = document.querySelector('button');
 btn.addEventListener('click', function () {
   clearCells();
+  deleteCells();
+  size = getSize();
+  newGrid(grid, size);
 });
 
