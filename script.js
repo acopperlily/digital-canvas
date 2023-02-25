36d03084456cad6ad5183971f248a951d0378c3a
acopperlily <april.copley@protonmail.com>
2023-02-25 01:58:26 -0500

    Working storage feature

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let allCells = [];
+
 // Set up and update canvas grid
 const Grid = (() => {
   let previousCell;
@@ -26,33 +28,53 @@ const Grid = (() => {
     currentSize.textContent = `${size} x ${size}`;
   };
 
-  const drawGrid = size => {
+  const drawGrid = (size=11) => {
+    allCells = [];
+    // let store = JSON.parse(window.localStorage.getItem('canvas'));
+    // if (store) size = store.length;
+    // console.log('store:', store.length);
     setSize(size);
+    // if (!store) store = '0';
     for (let i = 1; i <= size * size; i++) {
-      canvas.appendChild(createCell(i));
+      canvas.appendChild(createCell(i, '0'));
     }
     canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   };
 
   const deleteCells = () => {
+    allCells = [];
+    window.localStorage.clear();
     console.log('iife', canvas.lastElementChild);
     while (canvas.firstElementChild) {
       canvas.removeChild(canvas.firstElementChild);
     }
   };
 
-  drawGrid(11);
+  let store = JSON.parse(window.localStorage.getItem('canvas'));
+  if (store) {
+    
+    let size = store.length ** (1/2);
+    setSize(size);
+    slider.value = size;
+    for (let i = 1; i <= store.length; i++) {
+      canvas.appendChild(createCell(i, store[i-1]));
+    }
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  } else drawGrid(11);
+  console.log('cells array:', allCells);
 
   return {drawGrid, deleteCells, updatePrevCell};
 })();
 
 // Set up listeners for range slider and clear button
-const Controls = (() => {
-  const range = document.getElementById('slider');
-  const clearBtn = document.querySelector('button');
-  const getSize = () => range.value;
+(function () {
+  const slider = document.getElementById('slider');
+  const clearBtn = document.querySelector('.clearBtn');
+  const saveBtn = document.querySelector('.saveBtn');
 
-  range.oninput = () => {
+  const getSize = () => slider.value;
+
+  slider.oninput = () => {
     Grid.deleteCells();
     Grid.drawGrid(getSize());
   };
@@ -61,13 +83,23 @@ const Controls = (() => {
     Grid.deleteCells();
     Grid.drawGrid(getSize());
   });
+
+  saveBtn.addEventListener('click', () => {
+    console.log(allCells);
+    window.localStorage.setItem('canvas', JSON.stringify(allCells));
+  });
+
+  return { slider }
 })();
 
 // Create cell divs
-function createCell(index) {
+function createCell(index, opacity='0') {
   const cell = document.createElement('div');
   cell.classList.add('cell');
   cell.setAttribute('id', index);
+  console.log('opacity:', opacity);
+  cell.style.opacity = opacity;
+  allCells.push(opacity);
   return cell;
 }
 
@@ -75,10 +107,16 @@ function createCell(index) {
 function drawCell(cell) {
   // console.log('cell inside draw:', cell);
   Grid.updatePrevCell(cell.id);
+  let index = +(cell.id) - 1;
+  console.log('index:', index);
+  console.log('cell id?', allCells[index]);
   let opacity = getComputedStyle(cell).opacity;
   if (opacity == '1') {
     cell.style.opacity = '0';
   } else {
     cell.style.opacity = parseFloat(opacity) + 0.1;
   }
+  console.log(cell.style.opacity);
+  allCells[index] = getComputedStyle(cell).opacity;
 }
+
